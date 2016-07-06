a0ecfa3b766f4751a78c52f98190b09a0b3e1761
sartios <sartios@hotmail.com>
2016-07-06 19:05:15 +0300

    add TeamViewController specs

--- app/js/team/controllers.js
+++ app/js/team/controllers.js
@@ -1,115 +1,127 @@
-angular.module('team').controller('TeamListController', ['$scope','$location','TeamService',
- function($scope,$location,TeamService){
+angular.module('team').controller('TeamListController', ['$scope', '$location', 'TeamService',
+    function($scope, $location, TeamService) {
 
-   $scope.viewTeam = function(team){
-     console.log('viewTeam()');
-     $location.path('/team/view/'+ team.id);
-   };
+        $scope.viewTeam = function(team) {
+            console.log('viewTeam()');
+            $location.path('/team/view/' + team.id);
+        };
 
-  $scope.editTeam = function(team){
-    console.log('editTeam()');
-    $location.path('/team/edit/' + team.id);
-  };
+        $scope.editTeam = function(team) {
+            console.log('editTeam()');
+            $location.path('/team/edit/' + team.id);
+        };
 
-  $scope.deleteTeam = function(team){
-    console.log('deleteTeam('+team.id+')');
-    TeamService.delete(team.id);
-    $location.path('/teams');
-  };
+        $scope.deleteTeam = function(team) {
+            console.log('deleteTeam(' + team.id + ')');
+            TeamService.delete(team.id);
+            $location.path('/teams');
+        };
 
-  $scope.addTeam = function(){
-    $location.path('/team/new');
-  };
+        $scope.addTeam = function() {
+            $location.path('/team/new');
+        };
 
-  var init = function(){
-    $scope.teams = TeamService.getAll();
-  };
+        var init = function() {
+            $scope.teams = TeamService.getAll();
+        };
 
-  init();
-}]);
-
-angular.module('team').controller('TeamCreateController', ['$scope','$location','TeamService',
- function($scope,$location,TeamService){
-
-  $scope.createTeam = function(){
-    console.log('createTeam()');
-    var team = $scope.formNewTeam;
-    if(team){
-      team.players = [];
-      team = TeamService.create(team);
-      if(team){
-        $location.path('/teams');
-      }
+        init();
     }
-  };
+]);
 
-  var init = function(){
-  };
-  init();
-}]);
+angular.module('team').controller('TeamCreateController', ['$scope', '$location', 'TeamService',
+    function($scope, $location, TeamService) {
 
-angular.module('team').controller('TeamEditController', ['$scope','$location','$routeParams','TeamService','selectedTeam',
- function($scope,$location, $routeParams, TeamService, selectedTeam){
+        $scope.createTeam = function() {
+            console.log('createTeam()');
+            var team = $scope.formNewTeam;
+            if (team) {
+                team.players = [];
+                team = TeamService.create(team);
+                if (team) {
+                    $location.path('/teams');
+                }
+            }
+        };
 
-  $scope.updateTeam = function(){
-    console.log('updateTeam()');
-    var team = $scope.selectedTeam;
-    team.players = [];
-    console.log(team);
-    team = TeamService.update(team);
-    if(team){
-      $location.path('/teams');
+        var init = function() {};
+        init();
     }
-  };
+]);
 
-  var init = function(){
-    $scope.name = TeamService.get($routeParams.id).name;
-    if(selectedTeam){
-      $scope.selectedTeam = selectedTeam;
+angular.module('team').controller('TeamEditController', ['$scope', '$location', '$routeParams', 'TeamService', 'selectedTeam',
+    function($scope, $location, $routeParams, TeamService, selectedTeam) {
+
+        $scope.updateTeam = function() {
+            console.log('updateTeam()');
+            var team = $scope.selectedTeam;
+            if (team) {
+                team.players = [];
+                team = TeamService.update(team);
+                if (team) {
+                    $location.path('/teams');
+                }
+            }
+        };
+
+        var init = function() {
+            $scope.name = TeamService.get($routeParams.id).name;
+            if (selectedTeam) {
+                $scope.selectedTeam = selectedTeam;
+            }
+        };
+        init();
     }
-  };
-  init();
-}]);
+]);
 
 angular.module('team')
-  .controller('TeamViewController',['$scope', '$location', 'selectedTeam','PlayerPostService','FieldService',
-  'PlayGameInvitationService','JoinTeamInvitationService','CompanyService',
-  function($scope, $location, selectedTeam, PlayerPostService,
-     FieldService, PlayGameInvitationService, JoinTeamInvitationService, CompanyService){
+    .controller('TeamViewController', ['$scope', '$location', 'selectedTeam', 'PlayerPostService', 'FieldService',
+        'PlayGameInvitationService', 'JoinTeamInvitationService', 'CompanyService',
+        function($scope, $location, selectedTeam, PlayerPostService,
+            FieldService, PlayGameInvitationService, JoinTeamInvitationService, CompanyService) {
 
-    $scope.update = function(){
-      var post = $scope.post;
-      post.team = $scope.team;
-      post.field = FieldService.getById(post.field.id);
-      PlayerPostService.create(post);
-      $location.path('/home');
-    };
+            $scope.update = function() {
+                var post = $scope.post;
+                if (post) {
+                    var team = $scope.team;
+                    if (team) {
+                        post.team = team;
+                        var field = FieldService.getById(post.field.id);
+                        if(field){
+                            post.field = field;
+                            PlayerPostService.create(post);
+                            $location.path('/home');
+                        }
+                    }
+                }
+            };
 
-    $scope.viewPlayGameInvitations = function(){
-      $location.path('/play-game-invitations/' + selectedTeam.id);
-    };
+            $scope.viewPlayGameInvitations = function() {
+                $location.path('/play-game-invitations/' + selectedTeam.id);
+            };
 
-    var initPlayGameInvitations = function(){
-      var invitations = PlayGameInvitationService.getTeamInvitations(selectedTeam.id);
-      $scope.playGameInvitations = invitations.length;
-    };
+            var initPlayGameInvitations = function() {
+                var invitations = PlayGameInvitationService.getTeamInvitations(selectedTeam.id);
+                $scope.playGameInvitations = invitations.length;
+            };
 
-    var initPlayerInvitations = function(){
-      var invitations = JoinTeamInvitationService.getByTeam(selectedTeam.id);
-      $scope.playerInvitations = invitations.length;
-    };
+            var initPlayerInvitations = function() {
+                var invitations = JoinTeamInvitationService.getByTeam(selectedTeam.id);
+                $scope.playerInvitations = invitations.length;
+            };
 
-    var initAvailableCompanies = function(){
-      $scope.companies = CompanyService.getAll();
-    };
+            var initAvailableCompanies = function() {
+                $scope.companies = CompanyService.getAll();
+            };
 
-    var init = function(){
-      $scope.team = selectedTeam;
-      initPlayGameInvitations();
-      initPlayerInvitations();
-      initAvailableCompanies();
-    };
+            var init = function() {
+                $scope.team = selectedTeam;
+                initPlayGameInvitations();
+                initPlayerInvitations();
+                initAvailableCompanies();
+            };
 
-    init();
+            init();
 
-  }]);
+        }
+    ]);
--- spec/unit/team/controllers.spec.js
+++ spec/unit/team/controllers.spec.js
@@ -15,9 +15,9 @@ describe('Controllers', function(){
       mock.sampleTeams.push(mockTeam);
       mock.create = function(team){};
       mock.delete = function(team){};
-      mock.getAll = function(){
-        return mock.sampleTeams;
-      };
+      mock.get = function(id){return mock.sampleTeams[0];};
+      mock.update = function(team){return team;};
+      mock.getAll = function(){return mock.sampleTeams;};
 
       return mock;
     });
@@ -93,6 +93,175 @@ describe('Controllers', function(){
       $scope.createTeam();
       expect(teamService.create).not.toHaveBeenCalledWith($scope.formNewTeam);
       expect(location.path).not.toHaveBeenCalledWith('/teams');
+    });
+  });
+
+  describe('TeamEditController', function(){
+    var location, teamService, $scope, selectedTeam;
+    beforeEach(inject(function($rootScope,$controller,$location,TeamService,Team){
+      $scope = $rootScope.$new();
+      selectedTeam = mockTeam;
+      teamService = TeamService;
+      location = $location;
+      $controller('TeamEditController',{
+        $scope : $scope,
+        selectedTeam : selectedTeam,
+        $routeParams: {id : '1'}
+      });
+
+      spyOn(teamService, 'get').and.callThrough();
+      spyOn(teamService, 'update').and.callThrough();
+      spyOn(location, 'path');
+    }));
+
+    it('should have a team', function(){
+      expect($scope.name).toEqual(mockTeam.name);
+      expect($scope.selectedTeam).toEqual(mockTeam);
+    });
+
+    it('should update a team', function(){
+      $scope.updateTeam();
+      expect(teamService.update).toHaveBeenCalledWith($scope.selectedTeam);
+      expect(location.path).toHaveBeenCalledWith('/teams');
+    });
+
+    it('should not update a null team', function(){
+      $scope.selectedTeam = undefined;
+      $scope.updateTeam();
+      expect(teamService.update).not.toHaveBeenCalledWith($scope.selectedTeam);
+      expect(location.path).not.toHaveBeenCalledWith('/teams');
+    });
+
+    it('should not have an undefined team', function(){
+      expect($scope.name).toEqual(mockTeam.name);
+      expect($scope.selectedTeam).toEqual(mockTeam);
+    });
+
+  });
+
+  describe('TeamViewController', function(){
+    var $scope, location, selectedTeam,
+    playerPostService, fieldService, playGameInvitationService,
+    JoinTeamInvitationService, companyService, companies, field, spy;
+    beforeEach(module(function($provide){
+      $provide.factory('PlayerPostService', function(){
+        var mock = {};
+        mock.create = function(post){return post;};
+        return mock;
+      });
+
+      $provide.factory('FieldService', function(){
+        var mock = {};
+        field = {id : 1};
+        mock.getById = function(id){return field;};
+        return mock;
+      });
+
+      $provide.factory('PlayGameInvitationService', function(){
+        var mock = {};
+        mock.getTeamInvitations = function(id){return [{id: 1}];};
+        return mock;
+      });
+
+      $provide.factory('JoinTeamInvitationService', function(){
+        var mock = {};
+        mock.getByTeam = function(id){return [{id: 1}];};
+        mock.getAll = function(){return [{id: 1}];};
+        return mock;
+      });
+
+      $provide.factory('CompanyService', function(){
+        var mock = {};
+        companies = [];
+        companies.push({id: 1});
+        mock.getAll = function(){return companies;};
+        return mock;
+      });
+    }));
+
+    beforeEach(inject(
+      function($rootScope, $location, $controller, PlayerPostService,
+            FieldService, PlayGameInvitationService, JoinTeamInvitationService, CompanyService){
+          $scope = $rootScope.$new();
+          selectedTeam = mockTeam;
+          playerPostService = PlayerPostService;
+          fieldService = FieldService;
+          playGameInvitationService = PlayGameInvitationService;
+          joinTeamInvitationService = JoinTeamInvitationService;
+          companyService = CompanyService;
+          location = $location;
+          $controller('TeamViewController',{
+            $scope : $scope,
+            selectedTeam : selectedTeam
+          });
+
+          spyOn(fieldService,'getById').and.callThrough();
+          spyOn(playerPostService,'create').and.callThrough();
+          spyOn(playGameInvitationService,'getTeamInvitations').and.callThrough();
+          spyOn(joinTeamInvitationService,'getByTeam').and.callThrough();
+          spyOn(companyService,'getAll').and.callThrough();
+          spyOn(location, 'path');
+    }));
+
+    it('should have selected team', function(){
+      expect($scope.team).toEqual(selectedTeam);
+    });
+
+    it('should have play game invitations', function(){
+      expect($scope.playGameInvitations).toEqual(1);
+    });
+
+    it('should have player invitations', function(){
+      expect($scope.playerInvitations).toEqual(1);
+    });
+
+    it('should have available companies', function(){
+        expect($scope.companies).toEqual(companies);
+    });
+
+    it('should create a player post', function(){
+      $scope.post = {id : 1, field : field};
+      $scope.team = {id : 1};
+      var post = $scope.post;
+      post.team = $scope.team;
+      post.field = field;
+      $scope.update();
+      expect(fieldService.getById).toHaveBeenCalledWith(1);
+      expect(playerPostService.create).toHaveBeenCalledWith(post);
+      expect(location.path).toHaveBeenCalledWith('/home');
+    });
+
+    it('should not create an undefined player post', function(){
+      $scope.post = undefined;
+      $scope.team = {id : 1};
+      $scope.update();
+      expect(fieldService.getById).not.toHaveBeenCalledWith(1);
+      expect(playerPostService.create).not.toHaveBeenCalled();
+      expect(location.path).not.toHaveBeenCalledWith('/home');
+    });
+
+    it('should not create an undefined team player post', function(){
+      $scope.post = {id : 1, field : field};
+      $scope.team = undefined;
+      $scope.update();
+      expect(fieldService.getById).not.toHaveBeenCalledWith(1);
+      expect(playerPostService.create).not.toHaveBeenCalled();
+      expect(location.path).not.toHaveBeenCalledWith('/home');
+    });
+
+    it('should not create an undefined field player post', function(){
+      $scope.post = {id : 1, field : field};
+      $scope.team = {id : 1};
+      fieldService.getById.and.returnValue(false);
+      $scope.update();
+      expect(fieldService.getById).not.toHaveBeenCalledWith(1);
+      expect(playerPostService.create).not.toHaveBeenCalled();
+      expect(location.path).not.toHaveBeenCalledWith('/home');
+    });
+
+    it('should view play game invitations', function(){
+      $scope.viewPlayGameInvitations();
+      expect(location.path).toHaveBeenCalledWith('/play-game-invitations/' + selectedTeam.id);
     });
   });
 
