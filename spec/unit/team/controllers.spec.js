256d88ecb5bb884891df0015624e470a4f06a983
sartios <sartios@hotmail.com>
2016-07-06 12:38:16 +0300

    refactor TeamListController specs

--- spec/unit/team/controllers.spec.js
+++ spec/unit/team/controllers.spec.js
@@ -1,11 +1,11 @@
-describe('controller: TeamListController', function(){
-  var $scope;
+describe('Controllers', function(){
+
   beforeEach(module('app'));
 
   var mockTeam;
 
   beforeEach(module(function($provide){
-    $provide.factory('TeamService', function($q, Team){
+    $provide.factory('MockTeamService', function($q, Team){
       var mock = {};
       mock.sampleTeams = [];
         mockTeam = new Team({
@@ -26,46 +26,50 @@ describe('controller: TeamListController', function(){
     });
   }));
 
-  var location, teamService;
-  beforeEach(inject(function($rootScope,$controller,$location,TeamService){
-    $scope = $rootScope.$new();
-    location = $location;
-    teamService = TeamService;
-    $controller('TeamListController', {
-      $scope: $scope
+  describe('TeamListController', function(){
+    var location, teamService, $scope;
+    beforeEach(inject(function($rootScope,$controller,$location,MockTeamService){
+      $scope = $rootScope.$new();
+      location = $location;
+      teamService = MockTeamService;
+      $controller('TeamListController', {
+        $scope: $scope,
+        TeamService: MockTeamService
+      });
+
+      spyOn(location,'path');
+      spyOn(teamService, 'delete');
+    }));
+
+    it('should view a team', function(){
+      var team={};
+      team.id = 1;
+      $scope.viewTeam(team);
+      expect(location.path).toHaveBeenCalledWith('/team/view/' + team.id);
     });
 
-    spyOn(location,'path');
-    spyOn(teamService, 'delete');
-  }));
+    it('should edit a team', function(){
+      var team = {};
+      team.id = 1;
+      $scope.editTeam(team);
+      expect(location.path).toHaveBeenCalledWith('/team/edit/' + team.id);
+    });
 
-  it('should view a team', function(){
-    var team={};
-    team.id = 1;
-    $scope.viewTeam(team);
-    expect(location.path).toHaveBeenCalledWith('/team/view/' + team.id);
+    it('should delete a team', function(){
+      team = {};
+      team.id = 1;
+      $scope.deleteTeam(team);
+      expect(teamService.delete).toHaveBeenCalledWith( team.id);
+    });
+
+    it('should add a new team', function(){
+      $scope.addTeam();
+      expect(location.path).toHaveBeenCalledWith('/team/new');
+    });
+
+    it('should view all teams', function(){
+      expect($scope.teams[0]).toEqual(mockTeam);
+    });
   });
 
-  it('should edit a team', function(){
-    var team = {};
-    team.id = 1;
-    $scope.editTeam(team);
-    expect(location.path).toHaveBeenCalledWith('/team/edit/' + team.id);
-  });
-
-  it('should delete a team', function(){
-    team = {};
-    team.id = 1;
-    $scope.deleteTeam(team);
-    expect(teamService.delete).toHaveBeenCalledWith( team.id);
-  });
-
-  it('should add a new team', function(){
-    $scope.addTeam();
-    expect(location.path).toHaveBeenCalledWith('/team/new');
-  });
-
-  it('should view all teams', function(){
-    expect($scope.teams[0]).toEqual(mockTeam);
-  });
 });
